88770e6838a1351836864d9946c12d48eebc3ff7
Sam Krause <samuelnkrause@gmail.com>
2016-02-09 21:08:41 -0500

    added a second way to solve the problem

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-/*var cities = ["Warren", "Washington", "Wilkes-Barre", "Williamsport", "York"];
-
-for (var i = 0; i <= 4; i++) {
-    console.log(cities[i])
-};
-*/
-/*var citylist = cities.join();*/
-/*console.log(smallcities);*/
-
-/*var pets = ["cat", "dog", "elephant"];
-var filtered = pets.filter(function (pet) {
-  return (pet !== "elephant");
-});*/
-
-
+/*My first attempt filters the cities and returns
+only the ones that are less than seven characters.
+Then it joins them into a single string */
 var cities = ["Warren", "Washington", "Wilkes-Barre", "Williamsport", "York"];
-var small = cities.filter(function (city) {
+var short_cities = cities.filter(function (city) {
     return city.length < 7;
 });
-console.log(small.join());
+console.log(short_cities.join());
+
+
+/* My second attempt I uses a loop an pushes
+only the ones that are less than seven characters
+to a new array and then console logs the new array*/
+var cities = ["Warren", "Washington", "Wilkes-Barre", "Williamsport", "York"];
+var small = []
+function answer() {
+  for (i = 0; i < 5; i++){
+    if (cities[i].length < 7){
+      small.push(cities[i]);
+    }
+  };
+  return small;
+};
+console.log(answer());
